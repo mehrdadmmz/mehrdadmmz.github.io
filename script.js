6d503c77a79bfb7174259f2100bbd61e2c95d3f2
Mehrdad <106564562+mehrdadmmz@users.noreply.github.com>
2025-06-18 20:34:56 -0700

    JS file is now updated. Button will be interactive now.

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", () => {
+  history.replaceState({}, "", window.location.pathname);
+});
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
